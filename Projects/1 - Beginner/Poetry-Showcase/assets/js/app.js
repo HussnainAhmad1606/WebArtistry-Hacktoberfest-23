ab8b934b8b213e7403164c0c0b207b0f0ca1086d
SulagnaKundu <sulagnakundu7@gmail.com>
2023-10-22 21:23:19 -0700

    Adding Poetry Showcase Website

--- Projects/1 - Beginner/Poetry-Showcase/assets/js/app.js
+++ Projects/1 - Beginner/Poetry-Showcase/assets/js/app.js
@@ -1,1 +1,34 @@
-console.log('Hello World');
+randomNumber = Math.floor(Math.random()*4+1);  
+
+window.onload = function() {  
+	if (randomNumber == 1) {
+		document.getElementById("content1").style.display = "inline";
+		document.getElementById("content2").style.display = "none";
+		document.getElementById("content3").style.display = "none";
+		document.getElementById("content4").style.display = "none";
+	}
+	if (randomNumber == 2) {
+		document.getElementById("content1").style.display = "none";
+		document.getElementById("content2").style.display = "inline";
+		document.getElementById("content3").style.display = "none";
+		document.getElementById("content4").style.display = "none";
+	}
+	if (randomNumber == 3) {
+		document.getElementById("content1").style.display = "none";
+		document.getElementById("content2").style.display = "none";
+		document.getElementById("content3").style.display = "inline";
+		document.getElementById("content4").style.display = "none";
+	}
+	if (randomNumber == 4) {
+		document.getElementById("content1").style.display = "none";
+		document.getElementById("content2").style.display = "none";
+		document.getElementById("content3").style.display = "none";
+		document.getElementById("content4").style.display = "inline";
+	}
+}
+
+
+
+
+
+
